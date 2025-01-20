809d168c4e2819ab6a0c3fe163cfc6dbfddeb686
Mac <mac_pham@usiglobal.com>
2025-01-20 14:51:31 +0700

    first commit

---
+++ mysite/navigation/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=10000)
+    link = models.CharField(max_length=10000)
+    
+    class Meta:
+        db_table = 'pages'
+        
+    def __str__(self):
+        return self.title
+     
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    
+    class Meta:
+        db_table = 'users'
+    
+    def __str__(self):
+        return self.username
---
+++ mysite/navigation/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.user_view, name='user'),
+    path('login/', views.login_view, name='login'),
+    path('manage/', views.manage_view, name='manage'),
+]
